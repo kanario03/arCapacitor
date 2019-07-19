b8a89df73bfdc8d57b741d1680d4772d5e24c092
Luiz Viola <viola.luiz@hotmail.com>
2019-07-19 15:41:10 -0300

    plugin cam preview funcionando

--- pocAr/src/app/home/home.page.ts
+++ pocAr/src/app/home/home.page.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import {
+  CameraPreview,
+  CameraPreviewPictureOptions,
+  CameraPreviewOptions,
+  CameraPreviewDimensions
+} from '@ionic-native/camera-preview/ngx';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  providers: [CameraPreview]
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private camPreview: CameraPreview
+  ) { }
+
+  startCamera() {
+    // start camera
+    this.camPreview.startCamera(
+      {
+        x: 0,
+        y: 0,
+        width: window.screen.width,
+        height: window.screen.height,
+        camera: 'rear',
+        tapPhoto: true,
+        previewDrag: true,
+        toBack: true,
+        alpha: 1
+      }
+    ).then(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      });
+  }
 
 }
